406c174e81bc9c22b43e070f19a501e993ba204b
yranshin <yranshin@10.233.219.7>
2026-03-05 17:31:03 +0530

    Largest Digit program logic added

---
+++ JavaLigicBuilding/src/numbers/LargestDigit.java
@@ -0,0 +1,25 @@
+package numbers;
+import java.util.Scanner;
+
+public class LargestDigit {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a Number: ");
+		int Num=sc.nextInt();
+		int digit=0, max=0;
+		
+		while(Num>0)
+		{
+			digit=Num%10;
+			if(digit>max)
+			{
+				max=digit;
+			}
+			Num=Num/10;
+		}
+		System.out.print(max + " " + "Largest digit in the number.");
+		sc.close();
+	}
+
+}
